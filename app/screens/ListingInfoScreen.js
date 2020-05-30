686002e5a0b49f778d7cde9a8bb5c20de27bb860
emmyni <emmyni220@gmail.com>
2020-05-29 23:35:18 -0400

    Continue working on listing page

--- app/screens/ListingInfoScreen.js
+++ app/screens/ListingInfoScreen.js
@@ -12,87 +12,92 @@ import {
   Left,
   Body,
   Icon,
+  Item,
+  Input,
   Button,
+  Form,
   Right,
 } from "native-base";
 import { ScrollView } from "react-native-gesture-handler";
 
+import colors from "../config/colors";
+
 const ListingInfo = ({ route }) => {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [message, setMessage] = useState("Is this still available?");
   const { images } = route.params;
   const { item } = route.params;
-  const cards = [
-    {
-      text: "Card One",
-      name: "One",
-      image: require("../assets/chair.jpg"),
-    },
-    {
-      text: "Card Two",
-      name: "Two",
-      image: require("../assets/chair.jpg"),
-    },
-    {
-      text: "Card Three",
-      name: "Three",
-      image: require("../assets/chair.jpg"),
-    },
-  ];
 
-  const { height, width } = Dimensions.get("screen");
+  const sendMessage = () => {};
 
   return (
-    <Container>
-      <Card transparent>
-        <CardItem cardBody>
-          <ScrollView horizontal={true}>
-            {images.map((image, index) => {
-              return (
-                // <Card
-                //   transparent
-                //   key={index}
-                //   styles={{ margin: 0, padding: 0 }}
-                // >
-                //   <CardItem>
-                // <View styles={{ padding: 20 }}>
-                <Image
-                  key={index}
-                  source={{ uri: image }}
-                  style={{
-                    height: height / 2,
-                    width: width,
+    <ScrollView>
+      <Container>
+        <Card transparent>
+          <CardItem cardBody>
+            <ScrollView horizontal={true}>
+              {images.map((image, index) => {
+                return (
+                  <Image
+                    key={index}
+                    source={{ uri: image }}
+                    style={styles.image}
+                  />
+                );
+              })}
+            </ScrollView>
+          </CardItem>
 
-                    flex: 1,
-                  }}
-                />
-                // </View>
-                //   </CardItem>
-                // </Card>
-              );
-            })}
-          </ScrollView>
-        </CardItem>
-
-        <CardItem>
-          <Body>
-            <Text>{item.item_name}</Text>
-            <Text note>{item.desc}</Text>
-          </Body>
-        </CardItem>
-        <CardItem>
-          <Left>
-            <Thumbnail source={{ uri: "Image URL" }} />
+          <CardItem>
             <Body>
-              <Text>NativeBase</Text>
-              <Text note>GeekyAnts</Text>
+              <Text>{item.item_name}</Text>
+              <Text style={styles.priceText}>${item.price}</Text>
+              <Text note>{item.desc}</Text>
             </Body>
-          </Left>
-        </CardItem>
-      </Card>
-    </Container>
+          </CardItem>
+          <CardItem>
+            <Left>
+              <Thumbnail source={{ uri: "Image URL" }} />
+              <Body>
+                <Text>NativeBase</Text>
+                <Text note>GeekyAnts</Text>
+              </Body>
+            </Left>
+          </CardItem>
+        </Card>
+        <Form style={styles.form}>
+          <Item rounded style={styles.inputBox}>
+            <Input onChangeText={(text) => setMessage(text)}>
+              <Text>{message}</Text>
+            </Input>
+          </Item>
+
+          <Button rounded block onPress={() => sendMessage()}>
+            <Text>Message Seller</Text>
+          </Button>
+        </Form>
+        <View style={{ height: 800 }}></View>
+      </Container>
+    </ScrollView>
   );
 };
 
 export default ListingInfo;
 
-const styles = StyleSheet.create({});
+const { height, width } = Dimensions.get("screen");
+const styles = StyleSheet.create({
+  form: {
+    margin: 10,
+  },
+  image: {
+    height: height / 2,
+    width: width,
+    marginRight: 15,
+    flex: 1,
+  },
+  inputBox: {
+    marginBottom: 10,
+  },
+  priceText: {
+    color: colors.primary,
+  },
+});
