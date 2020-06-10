f0c90df59cdd348c7d9f876431a1dd0cd5f50e59
emmyni <emmyni220@gmail.com>
2020-06-09 21:26:46 -0400

    Install GiftedChat

--- app/screens/Profile/ChatScreen.js
+++ app/screens/Profile/ChatScreen.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default function ChatScreen() {
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      text: "Hello developer",
+      createdAt: new Date(),
+    },
+  ]);
+
   return (
-    <View>
-      <Text>Chat Screen</Text>
+    <View style={styles.container}>
+      <GiftedChat messages={messages} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
